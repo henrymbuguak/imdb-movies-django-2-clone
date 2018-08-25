6e0cf2aa47cd8efdee785cbd669008c7919b0402
henrymbuguak <henrymbuguak@gmail.com>
2018-08-25 11:54:00 +0300

    Navigation bar and movie detail page

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('movies', views.MovieList.as_view(), name='movie_list'),
+    path('movie/<int:pk>', views.MovieDetail.as_view(), name='movie_detail'),
 ]
 
