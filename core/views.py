2d7b36464aabcc994499fe0f25fa7c5d3a5d8156
henrymbuguak <henrymbuguak@gmail.com>
2018-08-25 09:23:15 +0300

    Adding model to admin and first view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from core.models import Movie
 
-# Create your views here.
+
+class MovieList(ListView):
+    model = Movie
+
