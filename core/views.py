d066dfe4aa4df928a3cd2d3ad328d08b42da4b36
henrymbuguak <henrymbuguak@gmail.com>
2018-08-25 11:10:00 +0300

    Changes on how we load base templates

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from core.models import Movie
 
 
 class MovieList(ListView):
+    model = Movie
+
+
+class MovieDetail(DetailView):
     model = Movie
 
