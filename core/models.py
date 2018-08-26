40e713fd938b422321e085074ee189eef0a957a9
henrymbuguak <henrymbuguak@gmail.com>
2018-08-26 10:55:27 +0300

    Additional model for Person and Role

--- core/models.py
+++ core/models.py
@@ -19,6 +19,9 @@ class Movie(models.Model):
     rating = models.IntegerField(choices=RATINGS, default=NOT_RATED)
     runtime = models.PositiveIntegerField()
     website = models.URLField(blank=True)
+    director = models.ForeignKey(to='Person', on_delete=models.SET_NULL, related_name='directed', blank=True, null=True)
+    writers = models.ManyToManyField(to='Person', related_name='writing_credits', blank=True)
+    actors = models.ManyToManyField(to='Person', through='Role', related_name='acting_credits', blank=True)
 
     class Meta:
         ordering = ('-year', 'title')
@@ -26,3 +29,39 @@ class Movie(models.Model):
     def __str__(self):
         return '{} ({})'.format(self.title, self.year)
 
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    born = models.DateField()
+    died = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('last_name', 'first_name')
+
+    def __str__(self):
+        if self.died:
+            return '{}, {} ({} - {})'.format(
+                self.last_name,
+                self.first_name,
+                self.born,
+                self.died
+            )
+
+        return '{}, {}, ({})'.format(
+            self.last_name,
+            self.first_name,
+            self.born
+        )
+
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.DO_NOTHING)
+    person = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{} {} {}".format(self.movie_id, self.person_id, self.name)
+
+    class Meta:
+        unique_together = ('movie', 'person', 'name')
