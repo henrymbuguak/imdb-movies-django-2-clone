80732e04f78b671742f98c6d401fd1363e2cd93f
henrymbuguak <henrymbuguak@gmail.com>
2018-08-26 12:00:52 +0300

    Creation of movie model manager

--- core/models.py
+++ core/models.py
@@ -9,6 +9,14 @@ class PersonManager(models.Manager):
         return qs.prefetch_related('directed', 'writing_credits', 'role_set__movie')
 
 
+class MovieManager(models.Manager):
+    def all_with_related_persons(self):
+        qs = self.get_queryset()
+        qs = qs.select_related('director')
+        qs = qs.prefetch_related('writers', 'actors')
+        return qs
+
+
 class Movie(models.Model):
     NOT_RATED = 0
     RATED_G = 1
@@ -30,6 +38,8 @@ class Movie(models.Model):
     director = models.ForeignKey(to='Person', on_delete=models.SET_NULL, related_name='directed', blank=True, null=True)
     writers = models.ManyToManyField(to='Person', related_name='writing_credits', blank=True)
     actors = models.ManyToManyField(to='Person', through='Role', related_name='acting_credits', blank=True)
+
+    objects = MovieManager()
 
     class Meta:
         ordering = ('-year', 'title')
--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ class MovieList(ListView):
 
 
 class MovieDetail(DetailView):
-    model = Movie
+    queryset = Movie.objects.all_with_related_persons()
 
 
 class PersonDetail(DetailView):
